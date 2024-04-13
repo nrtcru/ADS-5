ed049678c6325782326ab6b8aa7e3fe09847168e
nrtcru <94432719+nrtcru@users.noreply.github.com>
2024-04-13 17:26:35 +0300

    b54 update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,26 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+T data[size];
+	int top;
+public:
+	TStack() : top(0) {}
+	void push(const T& item) {
+		if (top <= size - 1)
+			data[top++] = item;
+	}
+	T pop() {
+		if (top > 0)
+			return data[--top];
+	}
+	bool isEmpty() {
+		return top == 0;
+	}
+	T get() {
+		if (!isEmpty())
+			return data[top - 1];
+	}
 };
 
 #endif  // INCLUDE_TSTACK_H_
